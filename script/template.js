11add9320fe5592d5ba66ad2f103c12a855c2591
neuralninja <ninjaneural@gmail.com>
2024-08-01 17:12:37 +0900

    update

--- script/template.js
+++ script/template.js
@@ -66,9 +66,21 @@ let checkpoints = [];
 async function copy_files() {
     let templateCode = fs.readFileSync(BASE_SRC, { encoding: 'utf8' });
     let readme = [];
-    readme.push(`| Colab                                                                                                                                                                                            | Model                                                                                  | VAE  | Memo                    |`);
-    readme.push(`| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | -------------------------------------------------------------------------------------- | ---- | ----------------------- |`);
-    const list = checkpoints.filter(x=>!x.auth).sort((a, b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 0);
+    readme.push(`| Colab                                                                                                                                                                                            | Model                                                                                  | VAE  | Memo                    | Model                  |`);
+    readme.push(`| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------ | -------------------------------------------------------------------------------------- | ---- | ----------------------- | ---------------------- |`);
+    const list = checkpoints.filter(x=>!x.auth).map(item=>{
+        let id = item.name.toLowerCase().replace(/[- ]/g, '_').replace(/[^a-z0-9]/g, '');
+        if (!item.ipynb) {
+            item.ipynb = id+'_webui_colab';
+        }
+        if (!item.checkpoint_file) {
+            item.checkpoint_file = id+'.safetensors';
+        }
+        if (!item.sdmodel) {
+            item.sdmodel = 'sdxl';
+        }
+        return item;
+    });
     default_checkpoints.forEach((item) => {
         console.log(`${item.ipynb} 복사`);
         let code = templateCode;
@@ -79,9 +91,23 @@ async function copy_files() {
         code = code.replaceAll('#preset_memo#', "#@markdown **<font color=blue>체크포인트 URL을 입력하면 입력한 체크포인트로 자동 변경해서 다운받아요</font>**");            
         fs.writeFileSync(`../${TARGET_DIR}/${item.ipynb}.ipynb`, code);
 
-        readme.push(`| [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/fooocus/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} |                       |`)
+        readme.push(`| [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge.svg)](https://colab.research.google.com/github/ninjaneural/fooocus/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} |                       | SDXL            |`)
     });
-    list.forEach((item) => {
+    list.sort((a, b) => {
+        // sdmodel 값을 기준으로 정렬
+        let align = 0;
+        if (a.sdmodel < b.sdmodel) align += 100;
+        if (a.sdmodel > b.sdmodel) align += -100;
+        
+        // sdmodel 값이 같으면 name 값을 기준으로 정렬
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+        
+        if (nameA < nameB) align += -10;
+        if (nameA > nameB) align += 10;
+        
+        return align;
+    }).forEach((item) => {
         console.log(`${item.ipynb} 복사`);
         let code = templateCode;
         templateDefault = 'default';
@@ -103,7 +129,11 @@ async function copy_files() {
         defaultCode = defaultCode.replaceAll('#Checkpoint_Filename#', item.checkpoint_file);        
         fs.writeFileSync(`../misc/presets/${item.ipynb}.json`, defaultCode);
 
-        readme.push(`| [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge-nightly.svg)](https://colab.research.google.com/github/ninjaneural/fooocus/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      |`)
+        if (item.sdmodel=='pony') {
+            readme.push(`| [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge-install.svg)](https://colab.research.google.com/github/ninjaneural/fooocus/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      | Pony           |`)
+        } else {
+            readme.push(`| [![Open In Colab](https://raw.githubusercontent.com/neuralninja22/colab/master/icons/colab-badge-nightly.svg)](https://colab.research.google.com/github/ninjaneural/fooocus/blob/master/${TARGET_DIR}/${item.ipynb}.ipynb) | [${item.name}](${item.model})                    | ${item.bakedVAE ? '' : '선택'} | ${item.type}                      | SDXL           |`)
+        }
     });
 
     readmeText = readme.join('\n');
